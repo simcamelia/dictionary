f4286031e700a9765d0702b0adb4314bd11b9628
simcamelia <elenaalungulesei@gmail.com>
2025-11-07 12:53:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from "react";
 import Dictionary from "./Dictionary.jsx";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <main>
-      <Dictionary />
-    </main>
+    <>
+      <main>
+        <Dictionary />
+      </main>
+
+      <footer className="site-footer">
+        This project was coded by{" "}
+        <a
+          href="https://github.com/simcamelia"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Camelia Simion
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/simcamelia/dictionary"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://dictionarycs.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
+    </>
   );
 }
-
-export default App;
