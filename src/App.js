2eee0f8e52bff27528eb8aff8b6c3accb19e588e
simcamelia <elenaalungulesei@gmail.com>
2025-11-07 10:55:50 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import logo from "./logo.png";
-import Dictionary from "./Dictionary";
-import "./App.css";
+import React from "react";
+import Dictionary from "./components/Dictionary";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
-            <img src={logo} className="logo" alt="SheCodes Logo" />
-          </a>
-        </header>
-        <Dictionary defaultKeyword="sunset" />
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://www.shecodes.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            SheCodes
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/shecodesio/dictionary"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://shecodes-dictionary.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </footer>
-      </div>
-    </div>
+    <main>
+      <Dictionary />
+    </main>
   );
 }
 
